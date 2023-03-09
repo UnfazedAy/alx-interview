5a0e45176f174daf82c55b514c58f1c8643bac89
UnfazedAy <ayomidesoniyi@gmail.com>
2023-03-09 21:07:48 +0100

    Done

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,23 +1,23 @@
 #!/usr/bin/node
-
-const argv = process.argv
+const argv = process.argv;
 const request = require('request');
 
 function getPromise (url) {
   const promise = new Promise(function (resolve, reject) {
-    request(url, function (err, res, body){
-        if (err) reject(err); else resolve(body);
+    request(url, function (err, res, body) {
+      if (err) reject(err);
+      else resolve(JSON.parse(body).name);
     });
   });
-  return promise
+  return promise;
 }
 
 request(`https://swapi-api.hbtn.io/api/films/${argv[2]}`, async function (err, res, body) {
   if (err) throw err;
   const data = JSON.parse(body).characters;
 
-  for (let i of data) {
-    const res = await getPromise(i)
-    console.log(res)
+  for (let i = 0; i < data.length; i++) {
+    const result = await getPromise(data[i]);
+    console.log(result);
   }
 });
